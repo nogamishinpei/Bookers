8dbd7af0fb76b46eefe47974b6957140e21c9d79
EC2 Default User <nogamichiru@gmail.com>
2021-07-11 08:18:24 +0000

    [Fix]syntaxエラー解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
+ def new
+    @book = Book.new
+
+ end
+  
   def index
+  @books = Book.all
   end
 
-  def show
-  end
 
-  def new
-  end
+   def create
+    # １. データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to 'top'
+   end
 
-  def edit
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
