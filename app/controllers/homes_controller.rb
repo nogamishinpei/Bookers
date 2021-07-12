3c7b7de00fb8fcc11fa99a00ef437b5abba5e4cd
EC2 Default User <nogamichiru@gmail.com>
2021-07-12 10:19:11 +0000

    [add]3-8削除機能実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,18 +1,3 @@
 class HomesController < ApplicationController
-  
-  def top
-  
-  end
-  
-  def index
-  end
 
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
  get 'books/:id' => 'books#show', as: 'show_book'
  get 'books/:id/edit'=>'books#edit',as: 'editbook'
  patch 'books/:id'=> 'books#update', as: 'update_book'
-
+ delete 'books/:id' => 'books#destroy', as: 'destroy'
 end
