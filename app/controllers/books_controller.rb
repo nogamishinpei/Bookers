87469ab8b7114c32e61ac8e36845b37c60f8ce20
EC2 Default User <nogamichiru@gmail.com>
2021-07-12 08:28:57 +0000

    [add]3-8投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,6 @@
 class BooksController < ApplicationController
  def new
     @book = Book.new
-
  end
 
   def create
@@ -12,15 +11,27 @@ class BooksController < ApplicationController
     # ３. トップ画面へリダイレクト
     redirect_to '/books'
   end
-  
+
   def index
       @books = Book.all
   end
-  
-  
-  
-  
-  
+
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+     book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+
+
 
   private
   # ストロングパラメータ
